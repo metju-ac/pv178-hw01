4bec43d1b4f3c1be0e727e9277cfd8dfc743afce
xklima2 <xklima2@fi.muni.cz>
2024-02-23 15:26:21 +0100

    Initialize repo

---
+++ Interfaces/IBattle.cs
@@ -0,0 +1,21 @@
+﻿namespace HW01_2024.Interfaces
+{
+    public interface IBattle
+    {
+        /// <summary>
+        /// Performs a battle between two trainers.
+        /// </summary>
+        /// <param name="player">Player trainer</param>
+        /// <param name="enemy">Enemy trainer</param>
+        /// <returns>Winner trainer</returns>
+        //TODO:
+
+        /// <summary>
+        /// Performs one round of a battle between two FImons.
+        /// </summary>
+        /// <param name="playerFImon">Player trainer's FImon</param>
+        /// <param name="enemyFImon">Enemy trainer's FImon</param>
+        /// <returns>Winner FImon</returns>
+        //TODO:
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HW01_2024
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
